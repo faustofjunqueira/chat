7922ad5ded87c9f6c7fa8e80963c09b88621dd72
Fausto Junqueira <faustoferjunqueira@gmail.com>
2015-11-01 20:43:37 -0200

    Criado estrutura do Cliente e Sala

--- src/chat/dominio/entidade/Sala.java
+++ src/chat/dominio/entidade/Sala.java
@@ -1,9 +1,30 @@
 package chat.dominio.entidade;
 
+import java.util.List;
+
 public class Sala extends Dominio {
 
+	private String nome;
+	private List<Usuario> usuario;
+	
 	public Sala() {
 		super();
 	}
 
+	public Sala(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public List<Usuario> getUsuarios(){
+		return usuario;
+	}
 }
--- src/chat/dominio/entidade/Usuario.java
+++ src/chat/dominio/entidade/Usuario.java
@@ -2,8 +2,22 @@ package chat.dominio.entidade;
 
 public class Usuario extends Dominio {
 
+	private String nome;
+
 	public Usuario() {
 		super();
 	}
-	
+
+	public Usuario(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
